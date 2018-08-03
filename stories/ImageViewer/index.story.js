89d6f09b813941578a9e38f1e67f2bb742c2869c
argshook <argshook@gmail.com>
2018-08-03 10:30:20 +0300

    docs(ImageViewer): use autodocs

---
+++ stories/ImageViewer/index.story.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import ImageViewer from 'wix-style-react/ImageViewer';
+
+import CodeExample from 'wix-storybook-utils/CodeExample';
+
+import ExampleStandard from './ExampleStandard';
+import ExampleStandardRaw from '!raw-loader!./ExampleStandard';
+
+const exampleImageUrl = [
+  {label: 'No Image', value: ''},
+  {
+    label: 'Image',
+    value: 'https://static.wixstatic.com/media/c78d05b79ede429fb77c9d8ec4443b93.jpg/v1/fit/w_375,h_375/c78d05b79ede429fb77c9d8ec4443b93.jpg'
+  }
+];
+
+export default {
+  category: '3. Inputs',
+  storyName: '3.10 ImageViewer',
+
+  component: ImageViewer,
+  componentPath: '../../src/ImageViewer',
+
+  componentProps: {
+    imageUrl: exampleImageUrl[0].value
+  },
+
+  exampleProps: {
+    imageUrl: exampleImageUrl,
+    onAddImage: () => 'onAddImage',
+    onUpdateImage: () => 'onUpdateImage',
+    onRemoveImage: () => 'onRemoveImage'
+  },
+
+  examples: (
+    <CodeExample title="Standard" code={ExampleStandardRaw}>
+      <ExampleStandard/>
+    </CodeExample>
+  )
+};
